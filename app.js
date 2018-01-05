2b7a5c13f8533e938d514a03c28d6cdbb29ff751
James <boydston.j@gmail.com>
2018-01-04 19:58:52 -0800

    background on win

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
-var form = document.querySelector("#guess");
-var input = document.querySelector('input');
-var response = document.querySelector('.response');
+var form = document.querySelector("#guess")
+var input = document.querySelector('input')
+var response = document.querySelector('.response')
 var correctAnswer = Math.ceil(Math.random() * 10)
+var guessList = []
+var guesses = document.querySelector('.guesses');
 
 console.log(correctAnswer);
 
@@ -12,7 +14,10 @@ function guess(e) {
   e.preventDefault()
   var theirAnswer = input.value
 
+  guessList.push(theirAnswer)
+
   if (theirAnswer == correctAnswer) {
+    setInterval(changeColor, 200)
     response.innerHTML = "Yay, You did it!"
   }
   else if (theirAnswer > correctAnswer) {
@@ -22,6 +27,17 @@ function guess(e) {
     response.innerHTML = "Too Small"
   }
   else {
+    guessList.pop()
     response.innerHTML = "Invalid Response"
   }
+
+  guesses.innerHTML = guessList
+}
+
+function changeColor() {
+  var red = Math.floor(Math.random() * 255)
+  var green = Math.floor(Math.random() * 255)
+  var blue = Math.floor(Math.random() * 255)
+
+  document.body.style.background = `rgb(${red}, ${blue}, ${green})`
 }
