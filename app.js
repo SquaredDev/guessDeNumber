b7cf70b91c7cde8d15517ffd650f813633538c5a
James <boydston.j@gmail.com>
2018-01-04 19:30:53 -0800

    math function added

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 
+var form = document.querySelector("#guess");
+var input = document.querySelector('input');
+var response = document.querySelector('.response');
+var correctAnswer = Math.ceil(Math.random() * 10)
+
+console.log(correctAnswer);
+
+form.addEventListener("submit", guess)
+
+function guess(e) {
+  e.preventDefault()
+  console.log('Hi Class');
+}
